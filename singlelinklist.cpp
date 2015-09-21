11c9c4ee25dca6062d7ef8c4a2fba22f87da030d
Fauzan Iraldi <aceancientime666@gmail.com>
2015-09-21 19:55:58 +0700

    Update singlelinklist.cpp
    input insertlast & deletelast

--- singlelinklist.cpp
+++ singlelinklist.cpp
@@ -1,7 +1,22 @@
 void insertfirst(list *l, address p);
-void insertlast(list *l, address p);
+void insertlast(list *l, address p){
+  address q = first(*l);
+    while (next(q) != nill){
+        q = next(q);
+    }
+    next(q)=p;
+};
 void insertafter(address q, address p);
 
 void deletefirst(list *l,address p);
-void deletelast(list *l,address p);
+void deletelast(list *l,address p){
+  address q;
+    p = first(*l);
+    while(next(p) != nill){
+        q = p;
+        p = next(p);
+    }
+    next(q)=nill;
+    dealokasi(p);
+};
 void deleteafter(address q,address *p);
